40d33458a0176e0ea6ecd931ef1233038cd77288
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-16 22:44:58 +0530

    UC-3-AddPartTime

--- Day3Assignment/Day3Assignment/Program.cs
+++ Day3Assignment/Day3Assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Constants
+int IS_PART_TIME = 1;
 int IS_FULL_TIME = 1;
 int EMP_RATE_PER_HOUR = 20;
 //Variables
@@ -6,8 +7,12 @@ int empHrs = 0;
 int empWage = 0;
 Random random = new Random();
 //Computation
-int empCheck = random.Next(0, 2);
-if (empCheck == IS_FULL_TIME)
+int empCheck = random.Next(0, 3);
+if (empCheck == IS_PART_TIME)
+{
+    empHrs = 4;
+}
+else if (empCheck == IS_FULL_TIME)
 {
     empHrs = 8;
 }
