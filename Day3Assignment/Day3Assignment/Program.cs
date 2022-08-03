6cd0aae5ba9e0f9cb96b57da4ccd33db2bd318f4
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-08-03 21:58:47 +0530

    Total Employee Wage

--- Day3Assignment/Day3Assignment/Program.cs
+++ Day3Assignment/Day3Assignment/Program.cs
@@ -1,6 +1,11 @@
 ﻿using Day3Assignment;
 
-  ClassMethod empWage = new ClassMethod (); 
-empWage.calculateEmpWage("Infocepts", 25, 25, 120); 
-empWage.calculateEmpWage("ABC", 30, 20, 150); 
-
+BuilderEmpWge bridgelabz = new BuilderEmpWge("Bridgelabz", 25, 25, 120); // creating a object of EmpWageBuilder class to access methods
+BuilderEmpWge reliance = new BuilderEmpWge("Reliance", 20, 20, 130); // creating a object of EmpWageBuilder class to access methods
+BuilderEmpWge abc = new BuilderEmpWge("ABC", 30, 20, 150); // creating a object of EmpWageBuilder class to access methods
+bridgelabz.calculateEmpWage();
+Console.WriteLine(bridgelabz.toString());
+reliance.calculateEmpWage();
+Console.WriteLine(reliance.toString());
+abc.calculateEmpWage();
+Console.WriteLine(abc.toString());
